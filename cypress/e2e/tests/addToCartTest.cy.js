ea3d2a3f08b792cbd3dd8c0c3d63bd6ce082dd82
RuslanG13 <ruslan.gudenko13@ukr.net>
2023-09-27 09:09:21 +0300

    Added commands.js and fix files path

--- cypress/e2e/tests/addToCartTest.cy.js
+++ cypress/e2e/tests/addToCartTest.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
-import { homePage } from "../pages/homePage";
+import { homePage } from "../../pages/homePage";
 const homePageObj = new homePage();
-import testData from "../fixtures/testData.json";
+import testData from "../../fixtures/testData.json";
 
 describe("test automation", () => {
 
